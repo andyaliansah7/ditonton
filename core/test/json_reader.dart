5979b8578ac26fff79d82aec337bbd4d266a553d
Andy Aliansah <andyaliansah7@gmail.com>
2024-02-24 11:28:59 +0700

    fix json_reader

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/core/test')) {
-    dir = dir.replaceAll('/core/test', '');
+  if (dir.endsWith('/test')) {
+    dir = dir.replaceAll('/test', '');
+  }
+  if (dir.endsWith('core')) {
+    return File('$dir/test/$name').readAsStringSync();
   }
   return File('$dir/core/test/$name').readAsStringSync();
 }
